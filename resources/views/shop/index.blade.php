49ca63993bbd2c5aecf0341785807d7a23c6523b
amine44482 <amine44482@gmail.com>
2019-07-30 16:54:24 +0100

    created the main project

---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('title')
+    Laravel shopping cart
+@endsection
+
+@section('content')
+<div class="row">
+        <div class="col-sm-6 col-md-4 img-thumbnail">
+          <div class="thumbnail clear-fix">
+            <img src="https://i.ytimg.com/vi/G2sv2SqTyJ4/maxresdefault.jpg" alt="blur" class='rounded mx-auto d-block' style='max-width:200px'>
+            <div class="caption">
+              <h3>Thumbnail label</h3>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+              <p><a href="#" class="btn btn-primary pull-right  " role="button">Button</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+@endsection
